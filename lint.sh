29ed567a8f62709795d336e66af05f0acfc14562
Zamiell <5511220+Zamiell@users.noreply.github.com>
2026-05-24 07:46:29 -0400

    chore: comments

--- lint.sh
+++ lint.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
+set -euo pipefail # Exit on errors and undefined variables.
+
+# Type-check Python
 uvx ty check .
+
+# Build C#
 dotnet build src/Sts2Emulator.Tests/Sts2Emulator.Tests.csproj --configuration Release
